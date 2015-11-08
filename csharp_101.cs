3a0f7858e058c2e22bcf577c76aead887caeb1a1
Ryan Shin <1982sdr@hanmail.net>
2015-11-08 16:38:51 +0900

    C# dump - add csharp_101.cs

---
+++ csharp_101.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace csharp_101
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			ArrayList array1 = new ArrayList ();
+			int var1 = 10;
+			int var2;
+			array1.Add (var1);
+			//var2 = ((List<int>)array1) [0];
+
+			//var2 = array1 [0];
+			//var2 = ((int[])array1)[0];
+			var2 = Convert.ToInt32 (array1 [0]);
+			Console.WriteLine (var2);
+		}
+	}
+}
